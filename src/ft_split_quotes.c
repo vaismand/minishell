668c9caec425cc935ea42aff2fa553fec616fa17
David <omgemjey@gmail.com>
2024-01-23 14:47:17 +0100

    Norminette ft_split_quotes.c 1.0v

--- src/ft_split_quotes.c
+++ src/ft_split_quotes.c
@@ -33,17 +33,6 @@ int	ft_count(char const *s, char c)
 	return (count);
 }
 
-/* static void	*ft_free(char **strs, int k)
-{
-	while (k >= 0)
-	{
-		free(strs[k]);
-		k--;
-	}
-	free(strs);
-	return (NULL);
-} */
-
 char	**ft_split_ignore_quotes(char const *s, char c)
 {
 	char	**strs;
@@ -59,42 +48,42 @@ char	**ft_split_ignore_quotes(char const *s, char c)
 	strs = (char **)malloc(sizeof(char *) * (ft_count(s, c) + 1));
 	if (!strs || !s)
 		return (NULL);
-	while (s[i]) 
+	while (s[i])
 	{
 		if (s[i] == '\'' || s[i] == '"')
 		{
 			inside_quotes = !inside_quotes;
 			if (inside_quotes == s[i] || inside_quotes == 0)
 			{
-                if (inside_quotes) {
+				if (inside_quotes)
 					inside_quotes = 0;
-				} else {
+				else
 					inside_quotes = s[i];
-				}
 			}
-        }
-		else if (!inside_quotes && s[i] == c) {
+		}
+		else if (!inside_quotes && s[i] == c)
+		{
 			if (i > j)
 			{
-				strs[k] = ft_substr(s, j, i - j);  // Duplicate the string from j to i
+				strs[k] = ft_substr(s, j, i - j);
 				k++;
 			}
-			j = i + 1;  // Start of next string
+			j = i + 1;
 		}
 		i++;
 	}
-	// Handle the last string
-	if (i > j) {
+	if (i > j)
+	{
 		strs[k] = ft_substr(s, j, i - j);
 		k++;
 	}
-	strs[k] = NULL;  // Null-terminate the array
-	return strs;
+	strs[k] = NULL;
+	return (strs);
 }
 
-int count_cmds(char **cmd) 
+int	count_cmds(char **cmd)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (cmd[i] != NULL && ft_strncmp(cmd[i], ">", 1) != 0 \
@@ -103,9 +92,9 @@ int count_cmds(char **cmd)
 	return (i);
 }
 
-int arr_len(char **arr) 
+int	arr_len(char **arr)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (arr[i])
@@ -113,40 +102,41 @@ int arr_len(char **arr)
 	return (i);
 }
 
-char *remove_outer_quotes(char *str) {
-    char *new_str;
-    char *i;
-    char *j;
-    char inside_quotes;
+char	*remove_outer_quotes(char *str)
+{
+	char	*new_str;
+	char	*i;
+	char	*j;
+	char	inside_quotes;
 
 	i = str;
 	inside_quotes = 0;
 	new_str = malloc(ft_strlen(str) + 1);
 	j = new_str;
-    while (*i != 0) {
-        if (*i == '\'' || *i == '"') {
-            if (inside_quotes == *i) {
-                inside_quotes = 0;
-            } else if (inside_quotes == 0) {
-                inside_quotes = *i;
-            } else {
-                *j++ = *i;
-            }
-        } else {
-            *j++ = *i;
-        }
-        i++;
-    }
-    *j = 0;
-
-    return new_str;
+	while (*i != 0)
+	{
+		if (*i == '\'' || *i == '"')
+		{
+			if (inside_quotes == *i)
+				inside_quotes = 0;
+			else if (inside_quotes == 0)
+				inside_quotes = *i;
+			else
+				*j++ = *i;
+		}
+		else
+			*j++ = *i;
+		i++;
+	}
+	*j = 0;
+	return (new_str);
 }
 
-char    *kv_strip_cmd(char *cmd)
+char	*kv_strip_cmd(char *cmd)
 {
-    char *new_cmd;
-	
+	char	*new_cmd;
+
 	new_cmd = remove_outer_quotes(cmd);
-    free(cmd);
-    return new_cmd;
+	free(cmd);
+	return (new_cmd);
 }
