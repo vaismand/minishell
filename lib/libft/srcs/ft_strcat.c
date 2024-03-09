c4bd854f4ae2e53ad0996d108d04c85e204f8cba
vaismand <vaisman.david91@gmail.com>
2024-03-09 11:05:39 +0100

    added extra functions to libft

---
+++ lib/libft/srcs/ft_strcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvaisman <dvaisman@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 15:38:44 by dvaisman          #+#    #+#             */
+/*   Updated: 2024/03/06 15:38:51 by dvaisman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcat(char *dst, const char *src)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i])
+		i++;
+	while (src[j])
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = src[j];
+	return (dst);
+}
